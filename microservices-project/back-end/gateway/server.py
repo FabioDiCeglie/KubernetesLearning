fda182960f6db48c0b1312c3e3bd64d1d6a8cdd5
FabioDiCeglie <dcfabio96@gmail.com>
2025-08-06 22:37:43 +0200

    feat: refactor server.py to support separate MongoDB connections for videos and MP3s, enhance upload and download functionality with authorization checks

--- microservices-project/back-end/gateway/server.py
+++ microservices-project/back-end/gateway/server.py
@@ -1,18 +1,18 @@
 import os, gridfs, pika, json
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, send_file
 from flask_pymongo import PyMongo
 
 from auth import validate
 from auth_service import access
 from storage import util
+from bson.objectid import ObjectId
 
 
 server = Flask(__name__)
-
-server.config['MONGO_URI'] = os.getenv("MONGO_URI") or "mongodb://host.minikube.internal:27017/videos"
-
-mongo = PyMongo(server)
-fs = gridfs.GridFS(mongo.db)
+mongo_video = PyMongo(server, uri=os.getenv("MONGO_URI") or "mongodb://host.minikube.internal:27017/videos")
+mongo_mp3 = PyMongo(server, uri=os.getenv("MONGO_URI") or "mongodb://host.minikube.internal:27017/mp3s")
+fs_videos = gridfs.GridFS(mongo_video.db)
+fs_mp3s = gridfs.GridFS(mongo_mp3.db)
 
 connection = pika.BlockingConnection(pika.ConnectionParameters("rabbitmq"))
 channel = connection.channel()
@@ -37,6 +37,9 @@ def login():
 def upload():
     access, err = validate.token(request)
 
+    if err:
+        return jsonify({'error': 'Not authorized'}), 401
+
     access = json.loads(access)
 
     if access["admin"]:
@@ -44,7 +47,7 @@ def upload():
             return "Exactly 1 file required", 400
         
         for _, f in request.files.items():
-            err = util.upload(f, fs, channel, access)
+            err = util.upload(f, fs_videos, channel, access)
 
             if err:
                 return err
@@ -56,7 +59,27 @@ def upload():
 
 @server.route('/download', methods=['GET'])
 def download():
-    pass
+    access, err = validate.token(request)
+
+    if err:
+        return jsonify({'error': 'Not authorized'}), 401
+
+    access = json.loads(access)
+
+    if access["admin"]:
+        file_id_string = request.args.get("file_id")
+
+        if not file_id_string:
+            return "File ID is required", 400
+
+        try:
+            out = fs_mp3s.get(ObjectId(file_id_string))
+
+            return send_file(out, download_name=f"{file_id_string}.mp3")
+        except Exception as err:
+            return jsonify({'error': str(err)}), 500
+    else:
+        return jsonify({'error': 'Not authorized'}), 401
 
 if __name__ == "__main__":
     server.run(host='0.0.0.0', port=8080)
